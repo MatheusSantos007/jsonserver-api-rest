d69114fa28f0666f31ac4a8ae72c21b53344e619
Matheus serafim <80589583+MatheusSantos007@users.noreply.github.com>
2024-09-23 22:10:22 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from "react"; // Importa React
+import ReactDOM from "react-dom"; // Importa ReactDOM para manipulação do DOM
+import App from "./App"; // Importa o componente App
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> {/* Renderiza o componente App dentro do modo estrito do React */}
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root") // Seleciona o elemento com id "root" no DOM para renderizar o App
 );
