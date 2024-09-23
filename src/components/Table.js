e79e41a3ada0ebd5f6db48cd540503020a461e64
Matheus serafim <80589583+MatheusSantos007@users.noreply.github.com>
2024-09-23 22:06:08 +0000

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,27 +1,29 @@
-import React from "react"
-import Form from "./Form"
+import React from "react" // Importa React
+import Form from "./Form" // Importa o componente Form
 
 const Table = ({ users, postUser, updateUser, deleteUser }) => {
+	// Função para mostrar/ocultar o formulário de atualização
 	const showUpdateUser = id => {
 		const form = document.getElementsByClassName(`show-form-${id}`)
 		form[0].classList.toggle("hide-form")
 	}
 
+	// Componente para renderizar uma linha da tabela
 	const Row = ({ user }) => {
 		return (
 			<>
 				<div className='row'>
-					<div>{user.name}</div>
-					<div>{user.email}</div>
-					<div>{user.phone}</div>
-					<div>{user.companies.name}</div>
+					<div>{user.name}</div> {/* Nome do usuário */}
+					<div>{user.email}</div> {/* Email do usuário */}
+					<div>{user.phone}</div> {/* Telefone do usuário */}
+					<div>{user.companies.name}</div> {/* Nome da empresa do usuário */}
 					<div className='buttons'>
-						<button onClick={() => showUpdateUser(user.id)}>Update</button>
-						<button onClick={() => deleteUser(user.id)}>Delete</button>
+						<button onClick={() => showUpdateUser(user.id)}>Update</button> {/* Botão para atualizar o usuário */}
+						<button onClick={() => deleteUser(user.id)}>Delete</button> {/* Botão para deletar o usuário */}
 					</div>
 				</div>
 				<div className={`hide-form show-form-${user.id}`}>
-					<Form userData={user} postUser={postUser} updateUser={updateUser} />
+					<Form userData={user} postUser={postUser} updateUser={updateUser} /> {/* Formulário para atualizar o usuário */}
 				</div>
 			</>
 		)
@@ -30,17 +32,17 @@ const Table = ({ users, postUser, updateUser, deleteUser }) => {
 	return (
 		<div className='table'>
 			<div className='titles'>
-				<div>Name</div>
-				<div>Email</div>
-				<div>Phone</div>
-				<div>Company</div>
-				<div>Actions</div>
+				<div>Name</div> {/* Título da coluna Nome */}
+				<div>Email</div> {/* Título da coluna Email */}
+				<div>Phone</div> {/* Título da coluna Telefone */}
+				<div>Company</div> {/* Título da coluna Empresa */}
+				<div>Actions</div> {/* Título da coluna Ações */}
 			</div>
 			<div className='rows'>
-				{users && users.map(u => <Row user={u} key={u.id} />)}
+				{users && users.map(u => <Row user={u} key={u.id} />)} {/* Renderiza uma linha para cada usuário */}
 			</div>
 		</div>
 	)
 }
 
-export default Table
+export default Table // Exporta o componente Table
