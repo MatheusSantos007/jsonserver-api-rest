c607cf1b89e1d17a3e1317a1a17cc858fbbd4e84
Matheus serafim <80589583+MatheusSantos007@users.noreply.github.com>
2024-09-23 22:07:06 +0000

    Update httpHelper.js

--- src/helpers/httpHelper.js
+++ src/helpers/httpHelper.js
@@ -1,50 +1,60 @@
+// Função helper para realizar requisições HTTP
 export const httpHelper = () => {
+	// Função customFetch para realizar a requisição com opções personalizadas
 	const customFetch = async (url, options = {}) => {
-		const defaultMethod = "GET"
+		const defaultMethod = "GET" // Método padrão é GET
 		const defaultHeaders = {
-			"Content-Type": "application/json",
+			"Content-Type": "application/json", // Cabeçalho padrão para JSON
 			Accept: "application/json",
 		}
-		const controller = new AbortController()
-		options.signal = controller.signal
+		const controller = new AbortController() // Controlador para abortar a requisição
+		options.signal = controller.signal // Adiciona o sinal de controle às opções
 
+		// Define o método e cabeçalhos da requisição
 		options.method = options.method || defaultMethod
 		options.headers = options.headers
 			? { ...defaultHeaders, ...options.headers }
 			: defaultHeaders
 
+		// Converte o corpo da requisição para JSON, se existir
 		options.body = JSON.stringify(options.body) || false
-		if (!options.body) delete options.body
+		if (!options.body) delete options.body // Remove o corpo se for falso
 
+		// Aborta a requisição após 3 segundos
 		setTimeout(() => {
 			controller.abort()
 		}, 3000)
 
 		try {
-			const response = await fetch(url, options)
-			return await response.json()
+			const response = await fetch(url, options) // Realiza a requisição
+			return await response.json() // Retorna a resposta em JSON
 		} catch (err) {
-			return err
+			return err // Retorna o erro em caso de falha
 		}
 	}
 
+	// Função para realizar requisição GET
 	const get = (url, options = {}) => customFetch(url, options)
 
+	// Função para realizar requisição POST
 	const post = (url, options) => {
 		options.method = "POST"
 		return customFetch(url, options)
 	}
 
+	// Função para realizar requisição PUT
 	const put = (url, options) => {
 		options.method = "PUT"
 		return customFetch(url, options)
 	}
 
+	// Função para realizar requisição DELETE
 	const del = (url, options) => {
 		options.method = "DELETE"
 		return customFetch(url, options)
 	}
 
+	// Retorna as funções de requisição
 	return {
 		get,
 		post,
